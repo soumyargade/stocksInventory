580cc41594e7fa7e307cf71f2ee879b020ac5f31
Soumya Gade <soumyargade@users.noreply.github.com>
2020-07-18 21:30:07 -0400

    Created table columns

--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -3,9 +3,12 @@ import { BehaviorSubject } from 'rxjs';
 
 export interface IStock {
   id: number;
+  ticker: string;
+  price: number;
+  previousPrice: number;
+  openingPrice: number;
+  percentProfit: number;
   name: string;
-  ticker: string
-  price: number
   description: string;
   lastUpdatedDate: string;
 }
@@ -22,41 +25,56 @@ export class StockService {
 
   stocks: IStock[] = [{
     id: generateId(), 
-    name: 'Tesla', 
     ticker: 'TSLA', 
     price: 1500.84, 
+    previousPrice: 1490.80, 
+    openingPrice: 1495.76, 
+    percentProfit: 0, 
+    name: 'Tesla', 
     description: 'Car company', 
     lastUpdatedDate: '7/17/20 7:59PM', 
   }, 
   {
     id: generateId(), 
-    name: 'Google', 
     ticker: 'GOOGL', 
     price: 1516.85, 
+    previousPrice: 1504.06, 
+    openingPrice: 1530.23, 
+    percentProfit: 0, 
+    name: 'Google', 
     description: 'Search engine', 
     lastUpdatedDate: '7/17/20 7:56PM'
   }, 
   {
     id: generateId(), 
-    name: 'Amazon', 
     ticker: 'AMZN', 
     price: 2961.97, 
+    previousPrice: 2899.90, 
+    openingPrice: 3120.03, 
+    percentProfit: 0, 
+    name: 'Amazon', 
     description: 'Online shopping', 
     lastUpdatedDate: '7/17/20 7:58PM'
   }, 
   {
     id: generateId(), 
-    name: 'Facebook', 
     ticker: 'FB', 
     price: 242.03, 
+    previousPrice: 243.65, 
+    openingPrice: 238.54, 
+    percentProfit: 0, 
+    name: 'Facebook', 
     description: 'Social media', 
     lastUpdatedDate: '7/17/20 7:59PM'
   }, 
   {
     id: generateId(), 
-    name: 'Microsoft', 
     ticker: 'MSFT', 
     price: 202.88, 
+    previousPrice: 206.89, 
+    openingPrice: 140.78, 
+    percentProfit: 0, 
+    name: 'Microsoft', 
     description: 'Computer company', 
     lastUpdatedDate: '7/17/20 7:59PM'
   }
